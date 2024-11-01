fb35705f1c9cf3ae8ed949cab8022572cfc337b1
black-adm <mattheuscontato17@gmail.com>
2024-11-01 11:29:43 -0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,12 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        "primary-black": "#000200",
+        "primary-white": "#FEFEFE",
+        "primary-gray": "#E0E0D3",
+        "primary-red": "#EC0000",
+        "primary-green": "#29BF12",
+        "primary-sky": "#2DC7FF",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },

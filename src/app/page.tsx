7e01ead86221afe01709349f444b8904acba8939
black-adm <mattheuscontato17@gmail.com>
2024-11-01 13:55:21 -0300

    fix: static header and footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
-import whiteLogo from "@/app/assets/images/white-logo.png";
-import Image from "next/image";
+import { Footer } from "@/components/shared/footer";
+import { Header } from "@/components/shared/header";
 
-export default function Home() {
+export default function LandingPage() {
   return (
-    <div className="bg-primary-black min-h-screen flex justify-center items-center">
-      <div className="flex flex-col items-center space-y-6">
-        <Image 
-          src={whiteLogo}
-          width={600}
-          height={300}
-          className="w-1/2"
-          alt="Logo da Coderaw"
-        />
-
-        <h1 className="text-5xl text-primary-white">
-          Seja bem vindo
-        </h1>
-      </div>
-    </div>
+    <>
+      <Header />
+      <Footer />
+    </>
   );
 }
---
+++ src/components/shared/footer.tsx
@@ -0,0 +1,179 @@
+import blackLogo from "@/app/assets/images/black-logo.png"
+import Image from "next/image"
+
+import {
+  FacebookIcon,
+  GithubIcon,
+  InstagramIcon,
+  LinkedinIcon
+} from "lucide-react"
+
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+import { Label } from "../ui/label"
+
+export function Footer() {
+  return (
+    <footer>
+      <div className="w-full space-y-16 py-16">
+        <div className="grid grid-cols-1 gap-12 sm:grid-cols-4 sm:gap-6">
+          <div>
+            <span className="font-medium text-foreground">
+              Empresa
+            </span>
+            <ul className="mt-4 list-inside space-y-4">
+              <li>
+                <a
+                  href="#"
+                  className="text-sm text-muted-foreground hover:text-primary"
+                >
+                  Sobre nós
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="text-sm text-muted-foreground hover:text-primary"
+                >
+                  Fundadores
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="text-sm text-muted-foreground hover:text-primary"
+                >
+                  Serviços
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <span className="text-sm font-medium text-foreground">
+              Produtos
+            </span>
+            <ul className="mt-4 list-inside space-y-4">
+              <li>
+                <a
+                  href="#"
+                  className="text-sm text-muted-foreground hover:text-primary"
+                >
+                  Condotec
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="text-sm text-muted-foreground hover:text-primary"
+                >
+                  Spendmanagement
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <span className="font-medium text-foreground">
+              Social
+            </span>
+            <ul className="mt-4 list-inside space-y-4">
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center gap-2 text-sm text-muted-foreground hover:text-primary"
+                >
+                  <GithubIcon className="size-4" />
+                  Github
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center gap-2 text-sm text-muted-foreground hover:text-primary"
+                >
+                  <LinkedinIcon className="size-4" />
+                  Linkedin
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center gap-2 text-sm text-muted-foreground hover:text-primary"
+                >
+                  <FacebookIcon className="size-4" />
+                  Facebook
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center gap-2 text-sm text-muted-foreground hover:text-primary"
+                >
+                  <InstagramIcon className="size-4" />
+                  Instagram
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <form className="w-full">
+              <a 
+                href="/" 
+                className="hidden sm:block"
+                aria-label="Coderaw logo"
+              >
+                <Image
+                  src={blackLogo}
+                  width={600}
+                  height={300}
+                  className="w-[60%] mb-2"
+                  alt="Ícone da empresa Coderaw"
+                />
+              </a>
+
+              <div className="flex flex-col space-y-3">
+                <Label
+                  className="font-medium text-foreground"
+                  htmlFor="current-email"
+                >
+                  Se inscreva na nossa newsletter
+                </Label>
+
+                <Input
+                  type="email"
+                  id="current-email"
+                  name="current-email"
+                  placeholder="Informe o seu e-mail"
+                  className="placeholder:text-xs"
+                  required
+                />
+
+                <Button type="button">
+                  Inscreva-se
+                </Button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div className="flex flex-col space-y-3 py-3 sm:flex-row sm:justify-between sm:items-center sm:space-y-0">
+          <span className="text-[11px] text-muted-foreground sm:text-sm">
+            © Coderaw Soluções Tecnológicas LTDA - 2024.
+          </span>
+
+          <span className="text-xs text-muted-foreground sm:text-sm">
+            Todos os direitos reservados.
+          </span>
+        </div>
+      </div>
+    </footer>
+  )
+}

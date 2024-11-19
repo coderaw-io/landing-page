8ef2ff671f638708d9aac4aaf12632e0e29c72df
black-adm <mattheuscontato17@gmail.com>
2024-11-19 18:06:28 -0300

    feat: boutique section and layout update

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-foreground hover:bg-cyan-300",
+        default: "bg-primary text-foreground hover:bg-cyan-300 dark:text-background",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:

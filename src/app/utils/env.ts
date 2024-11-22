45db903d5dc0f2f13d5b5f00d477afd129015a4c
black-adm <mattheuscontato17@gmail.com>
2024-11-22 13:58:55 -0300

    fix: corrections details

--- src/app/utils/env.ts
+++ src/app/utils/env.ts
@@ -2,8 +2,10 @@ import { z } from "zod"
 
 const envSchema = z.object({
   NEXT_PUBLIC_RESEND_API_KEY: z.string(),
+  NEXT_PUBLIC_RESEND_AUDIENCE_ID: z.string(),
 })
 
 export const env = envSchema.parse({
   NEXT_PUBLIC_RESEND_API_KEY: process.env.NEXT_PUBLIC_RESEND_API_KEY,
+  NEXT_PUBLIC_RESEND_AUDIENCE_ID: process.env.NEXT_PUBLIC_RESEND_AUDIENCE_ID,
 })
